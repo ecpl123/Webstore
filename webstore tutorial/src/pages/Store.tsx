d57eac453439c75d45b58b13f517c777b7f45cae
ecpl123 <ecygielski@gmail.com>
2023-07-08 09:50:31 -0500

    Restructured some files
    moved imgs public/imgs, added json for items, working on store functionality (added for loop which stringifys all items in the json file)

--- webstore tutorial/src/pages/Store.tsx
+++ webstore tutorial/src/pages/Store.tsx
@@ -1,3 +1,16 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+
 export function Store() {
-    return <h1>Store</h1>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row>
+        {storeItems.map((item) => (
+          <Col>{JSON.stringify(item)}</Col> //for loop that goes through each item in the items.jason file nd stringifys it in a Col component.
+        ))}
+        <Col></Col>
+      </Row>
+    </>
+  );
 }
