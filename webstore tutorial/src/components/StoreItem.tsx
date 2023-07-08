e4e70d06c0bafba7e97b7ab179a60d3c4fd56486
ecpl123 <ecygielski@gmail.com>
2023-07-08 16:52:42 -0500

    more work on shopping cart context
    the cards now have full add, remove functionality on button click.

--- webstore tutorial/src/components/StoreItem.tsx
+++ webstore tutorial/src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 type StoreItemProps = {
   id: number;
@@ -10,7 +11,15 @@ type StoreItemProps = {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 1;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
+
   return (
     // bootstrap card code with image body, title and add/remove from cart button divs
     <Card className="h-100">
@@ -30,7 +39,10 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         subsequent divs are for the add and remove buttons */}
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100"> + Add To Cart</Button>
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              {" "}
+              + Add To Cart
+            </Button>
           ) : (
             <div
               className="d-flex align-items-center flex-column"
@@ -40,13 +52,15 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 className="d-flex align-items-center justify-content-center"
                 style={{ gap: ".5rem" }}
               >
-                <Button>-</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                 <div>
                   <span className="fs-3">{quantity}</span> in cart
                 </div>
-                <Button>+</Button>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button variant="danger">remove</Button>
+              <Button variant="danger" onClick={() => removeFromCart(id)}>
+                remove
+              </Button>
             </div>
           )}
         </div>
--- webstore tutorial/src/context/ShoppingCartContext.tsx
+++ webstore tutorial/src/context/ShoppingCartContext.tsx
@@ -1,18 +1,88 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type ShoppingCartProviderProps = {
   children: ReactNode;
 };
 
-const ShoppingCartContext = createContext({});
+// using id here instead of item name allows us to simply look up the name/price info for that item, which allows us to update that info whenever.
+//CartItem is used later in an array of CartItem[]
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]); // figure out what this means
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0; // find item where the item.id === id, then IF we have the item, return the quantity, otherwise return a default value of 0
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 }; // ...item allows us to refer to the item currently looked at by the loop
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  //what is different between adding and removing?
+  //if I add an item it can start from 0, or already have an item. either way, it is going to be increased by 1
+  //if i remove an item it cannot start from 0 but it can go down to 0.
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id); //Array.filter by item where item.id !== id allows us to return a new array where the item with the current id is filtered out.
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id);
+    });
+  }
+
+  //value here should contain all method names defined in the ShoppingCartContext type
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
